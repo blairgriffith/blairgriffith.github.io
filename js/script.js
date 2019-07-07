5baa83923cddfa44374ea6c11b32b2a78d98ac5b
Blair <bgriffit@oberlin.edu>
2019-07-07 17:40:09 -0400

    reverted to separate functions for each project's details pop-up

--- js/script.js
+++ js/script.js
@@ -1,23 +1,36 @@
 var projects = document.getElementsByClassName('detail')
 
+console.log(projects.length)
+
 function getBox() {
+    var contentlist = [
+        'Virtual Pet is a console-based application that simulates a simple pet shelter. The user can add pets of two types (organic or robotic), each of which has a set of stats that correspond to its wellbeing. Users can then care for the pets in a variety of ways, affecting the stats of one or multiple pets.'
+        ,
+        'University Clinic Hospital is a console-based application that simulates a simple hospital staff and patient manager. The user can pay employees, choose medical employees to perform patient-related tasks, and list all employees or patients.'
+        ,
+        'Code Business Site is a single-page website utilizing HTML, CSS, Grid, and Flexbox to display a information for a fictional tech company.'
+        ]
     var boxbg = document.getElementById('boxbg');
     var box = document.getElementById('box');
-    var content = box.querySelector('p');
-    return { content, boxbg };
+    var content = document.getElementById('content');
+    return { content, boxbg, contentlist };
 }
 
-projects[0].addEventListener('click', function(){
-    var { content, boxbg } = getBox();
-    content.innerText = 'Virtual Pet is a console-based application that simulates a simple pet shelter. The user can add pets of two types (organic or robotic), each of which has a set of stats that correspond to its wellbeing. Users can then care for the pets in a variety of ways, affecting the stats of one or multiple pets.'
 
+projects[0].addEventListener('click', function(){
+    var { content, boxbg, contentlist } = getBox();
+    content.innerText = contentlist[0]
     boxbg.style.display = 'block';
 })
 
 projects[1].addEventListener('click', function(){
-    var {content, boxbg} = getBox();
-    content.innerText = 'University Clinic Hospital is a console-based application that simulates a simple hospital staff and patient manager. The user can pay employees, choose medical employees to perform patient-related tasks, and list all employees or patients.'
-
+    var {content, boxbg, contentlist } = getBox();
+    content.innerText = contentlist[1]
+    boxbg.style.display = 'block';
+})
+projects[2].addEventListener('click', function(){
+    var {content, boxbg, contentlist } = getBox();
+    content.innerText = contentlist[2]
     boxbg.style.display = 'block';
 })
 
